9437b026785b780d646a353c263adaa3a0642f2e
VladStorozhenko <43917096+VladStorozhenko@users.noreply.github.com>
2020-07-14 17:38:28 +0300

    Updates

---
+++ precache-manifest.783922d4efce82c462dbcb925b9f8e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aafff7b1d10918ebcc87f06226aaa655",
+    "url": "/reacttodo/index.html"
+  },
+  {
+    "revision": "2d21bfab36b1df0f9b01",
+    "url": "/reacttodo/static/css/main.c0b69af7.chunk.css"
+  },
+  {
+    "revision": "a1c5900e51c43a8a15f8",
+    "url": "/reacttodo/static/js/2.e8a7812c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reacttodo/static/js/2.e8a7812c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d21bfab36b1df0f9b01",
+    "url": "/reacttodo/static/js/main.ac19948d.chunk.js"
+  },
+  {
+    "revision": "4a8f6d0a237e7487a74d",
+    "url": "/reacttodo/static/js/runtime-main.f8ef5fb3.js"
+  }
+]);
